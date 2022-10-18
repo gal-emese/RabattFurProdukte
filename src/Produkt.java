0a1331391353d9836c192160791f1010347974c0
Emm <ese.mese02@gmail.com>
2022-10-18 11:44:13 +0300

    Initial commit

---
+++ src/Produkt.java
@@ -0,0 +1,9 @@
+public class Produkt {
+    double basisPrice;
+    int anzahlVonTage;
+
+    public Produkt(double basisPrice, int anzahlVonTage) {
+        this.basisPrice = basisPrice;
+        this.anzahlVonTage = anzahlVonTage;
+    }
+}
